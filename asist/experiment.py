0ec77513e1d14acfb25dffe88a4e078a274a6bd0
milancurcic <caomaco@gmail.com>
2018-10-05 14:30:16 -0400

    add Experiment and Run classes

---
+++ asist/experiment.py
@@ -0,0 +1,26 @@
+"""
+experiment.py
+"""
+from datetime import datetime
+from typing import List
+
+RunsList = List[Run]
+
+class Experiment():
+    def __init__(self: Experiment, name: str, runs: RunsList = []) -> None:
+        self.name = name
+        self.runs = runs
+
+    def add_run(self: Experiment, run: Run) -> None:
+        self.runs.append(run)
+
+class Run():
+    def __init__(self, start_time: datetime, end_time: datetime,
+                 fan: float = None, paddle_amplitude: float = None,
+                 paddle_frequency: float = None,
+                 water_depth: float = None) -> None:
+        self.start_time = start_time
+        self.end_time = end_time
+        self.fan = fan
+        self.paddle_amplitude = paddle_amplitude
+        self.paddle_frequency = paddle_frequency
