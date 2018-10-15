d48a9cb7449dde58d6bfde3c4b8f6fd5e81214d5
Milan Curcic <caomaco@gmail.com>
2018-10-15 17:55:20 -0400

    add reading hotfilm from netcdf

--- asist/hotfilm.py
+++ asist/hotfilm.py
@@ -2,7 +2,6 @@
 hotfilm.py
 """
 from datetime import datetime, timedelta
-from matplotlib.dates import date2num
 import numpy as np
 
 
@@ -25,11 +24,24 @@ def read_hotfilm_from_lvm(filename, dt=1e-3):
     seconds = float(line[5])
     useconds = int(1e6 * (seconds - int(seconds)))
     start_time = datetime(t[0], t[1], t[2], t[3], t[4], int(seconds), useconds)
-    time = date2num(start_time)
+    seconds = 0
     for line in data:
         line = line.split(',')[1:]
         ch1.append(float(line[6]))
         ch2.append(float(line[7]))
-        times.append(time)
-        time += dt / 86400
-    return times, ch1, ch2
+        seconds.append(time)
+        time += dt
+    return start_time, seconds, ch1, ch2
+
+
+def read_hotfilm_from_netcdf(filename):
+    """Reads 2-channel hotfilm data from a NetCDF file."""
+    with Dataset(filename, 'r') as nc:
+        seconds = nc.variables['Time'][:]
+        origin = datetime.strptime(nc.variables['Time'].origin, '%Y-%m-%d %H:%M:%S UTC')
+        time = np.array([origin + timedelta(seconds=seconds[n])\
+                                 for n in range(seconds.size)])
+        fan = nc.variables['fan'][:]
+        ch1 = nc.variables['ch1'][:]
+        ch2 = nc.variables['ch2'][:]
+    return time, fan, ch1, ch2
