0ce62be95411548c6b49da5cdafedc3f9a8808fe
Milan Curcic <caomaco@gmail.com>
2018-10-16 18:26:25 -0400

    datetime import

--- asist/pitot.py
+++ asist/pitot.py
@@ -1,6 +1,8 @@
 """
 pitot.py
 """
+from datetime import datetime, timedelta
+from netCDF4 import Dataset
 import numpy as np
 
 def pitot_velocity(dp, rho):
@@ -8,3 +10,13 @@ def pitot_velocity(dp, rho):
     rho [kg/m^3], returns the wind speed measured by the
     pitot tube."""
     return np.sqrt(2 * dp / rho)
+
+
+def read_pitot_from_netcdf(filename):
+    """Reads pitot data from a NetCDF file."""
+    with Dataset(filename, 'r') as nc:
+        seconds = nc.variables['Time'][:]
+        origin = datetime.strptime(nc.variables['Time'].origin, '%Y-%m-%dT%H:%M:%S')
+        fan = nc.variables['fan'][:]
+        u = nc.variables['u'][:]
+    return origin, seconds, fan, u
