39dddc7cbc66d0de153e1656c3852c04293b7e38
Milan Curcic <caomaco@gmail.com>
2018-10-16 17:45:46 -0400

    changes to pressure reader

--- asist/pressure.py
+++ asist/pressure.py
@@ -3,14 +3,26 @@ pressure.py
 """
 from datetime import datetime, timedelta
 import numpy as np
+import os
 
-def read_pressure_from_toa5(filename):
-    """Reads pressure data from TOA5 file written by
-    the Campbell Scientific logger."""
-    dp = []
-    times = []
-    data = [line.rstrip() for line in open(filename).readlines()]
-    for line in data[4:]:
+def read_pressure_from_toa5(filenames):
+    """Reads MKS pressure difference data from TOA5 file written by
+    the Campbell Scientific logger. If filenames is a string, 
+    process a single file. If it is a list of strings, 
+    process files in order and concatenate."""
+    if type(filenames) is str:
+        print('Reading ', filenames)
+        data = [line.rstrip() for line in open(filenames).readlines()[4:]]
+    elif type(filenames) is list:
+        data = []
+        for filename in filenames:
+            print('Reading ', os.path.basename(filename))
+            data += [line.rstrip() for line in open(filename).readlines()[4:]]
+    else:
+        raise RuntimeError('filenames must be string or list')
+
+    dp, times = [], []
+    for line in data:
         line = line.split(',')
         t = line[0].replace('"','')
         if len(t) == 19:
