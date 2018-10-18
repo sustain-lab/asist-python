c21d03793ee98186737be4f7547700f8057d25f6
Milan Curcic <caomaco@gmail.com>
2018-10-18 13:15:31 -0400

    update hotfilm and utility functions

--- asist/utility.py
+++ asist/utility.py
@@ -1,7 +1,13 @@
 """
-utility.py
+asist.utility -- Various utility functions used to process data.
 """
 import numpy as np
+
+
+def binavg(x, binsize):
+    """Simple binned average over binsize elements."""
+    return np.array([np.mean(x[n:n+binsize])\
+        for n in range(0, len(x), binsize)])
 
 
 def cat_numpy_arrays(arrays):
@@ -12,6 +18,19 @@ def cat_numpy_arrays(arrays):
     return np.array(x)
 
 
+def limit_to_percentile_range(x, plow, phigh):
+    """Limits the values of x to low and high percentile limits."""
+    xlow, xhigh = np.percentile(x, plow), np.percentile(x, phigh)
+    x[x < xlow] = xlow
+    x[x > xhigh] = xhigh
+    return x
+
+
 def deg2rad(d):
-    """Degrees to radians."""
+    """Degrees -> radians."""
     return d * np.pi / 180
+
+
+def running_mean(x, n):
+    """Running mean with the window n."""
+    return np.convolve(x, np.ones((n,)) / n, mode='same')
