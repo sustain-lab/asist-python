bf0f65e168d3246b7dc7fb4d3c042df52734ac42
milancurcic <caomaco@gmail.com>
2019-03-08 12:43:57 -0500

    unit tests for blackman-harris window function

--- asist/tests.py
+++ asist/tests.py
@@ -1,4 +1,12 @@
 """
 tests.py
 """
-#TODO
+from asist.utility import blackman_harris
+import numpy as np
+
+def test_blackman_harris():
+    N = 100
+    bh = blackman_harris(N)
+    assert type(bh) is np.ndarray
+    assert bh.size == N
+    assert np.max(bh) == 1
