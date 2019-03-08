7e5e55815b63588e297bc9a06465de37bedee289
milancurcic <caomaco@gmail.com>
2019-03-08 12:36:02 -0500

    add blackman-harris window function

--- asist/utility.py
+++ asist/utility.py
@@ -9,6 +9,14 @@ def binavg(x, binsize):
     return np.array([np.mean(x[n:n+binsize])\
         for n in range(0, len(x), binsize)])
 
+def blackman_harris(n):
+    """Returns the n-point Blackman-Harris window."""
+    x = np.linspace(0, 2 * np.pi, n, endpoint=False)
+    p = [0.35875, -0.48829, 0.14128, -0.01168]
+    res = np.zeros((n))
+    for i in range(4):
+        res += p[i] * np.cos(i * x)
+    return res
 
 def cat_numpy_arrays(arrays):
     """Concatenates numpy arrays."""
@@ -17,7 +25,6 @@ def cat_numpy_arrays(arrays):
         x += list(a)
     return np.array(x)
 
-
 def limit_to_percentile_range(x, plow, phigh):
     """Limits the values of x to low and high percentile limits."""
     xlow, xhigh = np.percentile(x, plow), np.percentile(x, phigh)
@@ -25,11 +32,9 @@ def limit_to_percentile_range(x, plow, phigh):
     x[x > xhigh] = xhigh
     return x
 
-
 def deg2rad(d):
     """Degrees -> radians."""
     return d * np.pi / 180
-
 
 def running_mean(x, n):
     """Running mean with the window n."""
