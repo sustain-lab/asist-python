9232b3450a2287cb64d58759be9c5cf5fe6b8708
milancurcic <caomaco@gmail.com>
2019-03-21 16:33:34 -0400

    add optional bin-averaging for power- and cross-spectrum functions

--- asist/utility.py
+++ asist/utility.py
@@ -4,10 +4,12 @@ asist.utility -- Various utility functions used to process data.
 import numpy as np
 from scipy.signal import detrend
 
+
 def binavg(x, binsize):
     """Simple binned average over binsize elements."""
     return np.array([np.mean(x[n:n+binsize])\
         for n in range(0, len(x), binsize)])
+
 
 def blackman_harris(n):
     """Returns the n-point Blackman-Harris window."""
@@ -18,12 +20,14 @@ def blackman_harris(n):
         res += p[i] * np.cos(i * x)
     return res
 
+
 def cat_numpy_arrays(arrays):
     """Concatenates numpy arrays."""
     x = []
     for a in arrays:
         x += list(a)
     return np.array(x)
+
 
 def limit_to_percentile_range(x, plow, phigh):
     """Limits the values of x to low and high percentile limits."""
@@ -32,27 +36,54 @@ def limit_to_percentile_range(x, plow, phigh):
     x[x > xhigh] = xhigh
     return x
 
+
 def deg2rad(d):
     """Degrees -> radians."""
     return d * np.pi / 180
+
 
 def running_mean(x, n):
     """Running mean with the window n."""
     return np.convolve(x, np.ones((n,)) / n, mode='same')
 
-def power_spectrum(x, dt):
-    """Power spectrum of x with a sampling interval dt."""
+
+def power_spectrum(x, dt, binsize=1):
+    """Power spectrum of x with a sampling interval dt.
+    Optionally, average over binsize if provided."""
+
+    assert type(x) is np.ndarray, 'x must be a numpy.ndarray'
+    assert dt > 0, 'dt must be > 0'
+    assert type(binsize) is int, 'binsize must be an int'
+    assert binsize > 0, 'binsize must be > 0'
+
     N = x.size
     window = blackman_harris(N)
     Sx = np.fft.fft(window * detrend(x))[:N//2]
+    C = dt / (np.pi * np.sum(window**2))
     df = 2 * np.pi / (dt * N)
     f = np.array([i * df for i in range(N//2)]) / (2 * np.pi)
-    C = dt / (np.pi * np.sum(window**2))
-    Sxx = 2 * np.pi * C * np.abs(Sx)**2
+
+    if binsize > 1:
+        Sxx = 2 * np.pi * C * binavg(np.abs(Sx)**2, binsize)
+        f = binavg(f, binsize)
+        df *= binavg
+    else:
+        Sxx = 2 * np.pi * C * np.abs(Sx)**2
+
     return Sxx, f, df
 
-def cross_spectrum(x, y, dt):
-    """Cross spectrum of x and y with a sampling interval dt."""
+
+def cross_spectrum(x, y, dt, binsize=1):
+    """Cross spectrum of x and y with a sampling interval dt.
+    Optionally, average over binsize if provided."""
+
+    assert type(x) is np.ndarray, 'x must be a numpy.ndarray'
+    assert type(y) is np.ndarray, 'y must be a numpy.ndarray'
+    assert x.size == y.size, 'x and y must have same size'
+    assert dt > 0, 'dt must be > 0'
+    assert type(binsize) is int, 'binsize must be an int'
+    assert binsize > 0, 'binsize must be > 0'
+
     N = x.size
     window = blackman_harris(N)
     Sx = np.fft.fft(window * detrend(x))[:N//2]
@@ -60,9 +91,19 @@ def cross_spectrum(x, y, dt):
     df = 2 * np.pi / (dt * N)
     f = np.array([i * df for i in range(N//2)]) / (2 * np.pi)
     C = dt / (np.pi * np.sum(window**2))
-    Sxx = 2 * np.pi * C * np.abs(Sx)**2
-    Syy = 2 * np.pi * C * np.abs(Sy)**2
-    Sxy = 2 * np.pi * C * np.conj(Sx) * Sy
+
+    if binsize > 1:
+        Sxx = 2 * np.pi * C * binavg(np.abs(Sx)**2, binsize)
+        Syy = 2 * np.pi * C * binavg(np.abs(Sy)**2, binsize)
+        Sxy = 2 * np.pi * C * binavg(np.conj(Sx) * Sy, binsize)
+        f = binavg(f, binsize)
+        df *= binavg
+    else:
+        Sxx = 2 * np.pi * C * np.abs(Sx)**2
+        Syy = 2 * np.pi * C * np.abs(Sy)**2
+        Sxy = 2 * np.pi * C * np.conj(Sx) * Sy
+
     phase = np.arctan2(-np.imag(Sxy), np.real(Sxy))
     coherence = np.abs(Sxy / np.sqrt(Sxx * Syy))
+
     return Sxx, Syy, Sxy, phase, coherence, f, df
